f053fc8ddc355d7f55470181a6308a81a285dc62
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 03:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ function prependKitten(name) {
 
 
 function removeLastKitten() {
-   kittens.slice(-1)
+   kittens.slice(-2)
    return kittens
   
 }
 
 function removeFirstKitten() {
-   return kittens.slice(0)
+   return kittens.slice(1)
 
   
 }
