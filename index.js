b571a0559f333635a66b169581696a533567f4e6
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 02:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Add your functions and code here
-function destructivelyAppendKitten(name) {
-  return name.slice(-1)
+function destructivelyAppendKitten(name, kitten) {
+return [...name, kitten]
 }
